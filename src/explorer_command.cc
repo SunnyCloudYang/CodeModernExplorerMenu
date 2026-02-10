8786ae7a992a820920c3fd65006cfa7686935cc2
GoodNullName <sunnycloudyang@outlook.com>
2026-02-10 14:31:59 +0800

    fix: fix path

--- src/explorer_command.cc
+++ src/explorer_command.cc
@@ -141,7 +141,7 @@ class __declspec(uuid(DLL_UUID)) ExplorerCommandHandler final : public RuntimeCl
 
   IFACEMETHODIMP GetIcon(IShellItemArray* items, PWSTR* icon) {
     std::filesystem::path module_path{ wil::GetModuleFileNameW<std::wstring>(wil::GetModuleInstanceHandle()) };
-    module_path = module_path.remove_filename().parent_path();
+    module_path = module_path.remove_filename().parent_path().parent_path();
     module_path = module_path / DIR_NAME / EXE_NAME;
 
     if (!std::filesystem::exists(module_path)) {
@@ -192,7 +192,7 @@ class __declspec(uuid(DLL_UUID)) ExplorerCommandHandler final : public RuntimeCl
   IFACEMETHODIMP Invoke(IShellItemArray* items, IBindCtx* bindCtx) {
       if (items) {
           std::filesystem::path module_path{ wil::GetModuleFileNameW<std::wstring>(wil::GetModuleInstanceHandle()) };
-          module_path = module_path.remove_filename().parent_path();
+          module_path = module_path.remove_filename().parent_path().parent_path();
           module_path = module_path / DIR_NAME / EXE_NAME;
 
           if (!std::filesystem::exists(module_path)) {
